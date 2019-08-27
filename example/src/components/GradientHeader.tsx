04ff1bbe549a69d3f508dcb437652ac8a89c842d
Alex Rodin <contato@alexrodin.info>
2019-08-27 20:28:13 -0300

    Adicionado efeitos especiais. Documentação inicial de todos os componentes

--- example/src/components/GradientHeader.tsx
+++ example/src/components/GradientHeader.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Animated, ImageBackground, StyleSheet,} from 'react-native';
+import {Animated, Dimensions, ImageBackground, StyleSheet, View,} from 'react-native';
 import {LayoutEvent} from 'react-navigation';
 import {EmptyState, getTheme, SimpleText, TableView, TableViewProps} from "rn-components-ui";
 
@@ -8,15 +8,59 @@ export type GradientHeaderProps = TableViewProps & {
     description: string | JSX.Element;
 }
 
-type IndexPageState = {
+type GradientHeaderState = {
     headerHeight: number;
+    particles: Array<JSX.Element>;
 }
 
-export default class GradientHeader extends React.PureComponent<GradientHeaderProps> {
+const randomBetween = (min: number, max: number) => {
+    return Math.random() * (max - min) + min;
+};
+
+const randomBetweenInt = (min: number, max: number) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const width = Dimensions.get('window').width;
+
+const PARTICLES_TEXTURES = [
+    require('../assets/particle-1.png'),
+    require('../assets/particle-2.png'),
+    require('../assets/particle-4.png'),
+];
+
+const PARTICLES_COLORS = [
+    '#C8ACFF',
+    '#7198FF',
+    '#5F30C2',
+    '#29A3AE',
+    '#FFC96F',
+    '#AE367E',
+    '#E9F097',
+    '#D956CA'
+];
+
+let PARTICLE_SEQ = 0;
+
+// Número máximo de particulas vivas
+const MAX_PARTICLES = 20;
+
+// Duração da animação
+const ANIM_DURATION = 10 * 1000;
+
+
+export default class GradientHeader extends React.PureComponent<GradientHeaderProps, GradientHeaderState> {
 
     state = {
-        headerHeight: 10
+        headerHeight: 10,
+        particles: []
     };
+
+    private stop: boolean = false;
+
+    private allParticles: Array<any> = [];
+
+    private animations: Array<Animated.CompositeAnimation> = [];
 
     private animatedScrollValue = new Animated.Value(0);
 
@@ -25,6 +69,155 @@ export default class GradientHeader extends React.PureComponent<GradientHeaderPr
             headerHeight: event.nativeEvent.layout.height
         })
     };
+
+    private addParticles = () => {
+        if (this.stop) {
+            return;
+        }
+
+        let now = +new Date();
+
+        // Remove da listagem as particulas mortas
+        this.allParticles = this.allParticles.filter(value => {
+            return value[1] > now;
+        });
+
+        // Mantém as particulas vivas
+        let particles: Array<JSX.Element> = this.allParticles.map(value => {
+            return value[0];
+        });
+
+        // Substitui particulas mortas
+        if (particles.length < MAX_PARTICLES) {
+            let diff = MAX_PARTICLES - particles.length;
+            let animationValue = new Animated.Value(0);
+
+            let minTTL = Number.MAX_SAFE_INTEGER;
+
+            for (let a = 0; a < diff; a++) {
+
+                // Atraso no inicio da animacao
+                let delay = randomBetween(0.0001, 0.01);
+
+                let maxEnergy = delay + randomBetween(0.3, 0.6);
+
+                // Tempo de vida
+                let ttl = randomBetween(maxEnergy * 1.1, 1 - delay);
+
+                // Tamanho inicial
+                let minSize = randomBetween(0.5, 1);
+
+                // Tamanho final
+                let maxSize = randomBetween(minSize, 2);
+
+                let txStart = Math.floor(randomBetween(-(width / 2), width / 2));
+                let txEnd = Math.floor(randomBetween(-(width / 2), width / 2));
+
+                let tyStart = Math.floor(randomBetween(-(width / 4), this.state.headerHeight / 2));
+                let tyEnd = Math.floor(randomBetween(-(width / 4), this.state.headerHeight / 2));
+
+                let texture = PARTICLES_TEXTURES[randomBetweenInt(0, PARTICLES_TEXTURES.length - 1)];
+                let color = PARTICLES_COLORS[randomBetweenInt(0, PARTICLES_COLORS.length - 1)];
+
+                let size = randomBetweenInt(width * 0.4, width * 0.7);
+
+                let particle = (
+                    <Animated.Image
+                        key={`p-${PARTICLE_SEQ++}`}
+                        source={texture}
+                        pointerEvents={'none'}
+                        style={{
+                            // borderWidth: 1,
+                            // borderColor: '#000',
+                            position: 'absolute',
+                            width: size,
+                            height: size,
+                            top: 0,
+                            left: 0,
+                            tintColor: color,
+                            opacity: animationValue.interpolate({
+                                inputRange: [0, delay, maxEnergy, ttl],
+                                outputRange: [0, 0, randomBetween(0.3, 0.8), 0]
+                            }),
+                            transform: [
+                                {
+                                    scale: animationValue.interpolate({
+                                        inputRange: [delay, ttl],
+                                        outputRange: [minSize, maxSize],
+                                        extrapolate: 'clamp'
+                                    })
+                                },
+                                {
+                                    translateY: animationValue.interpolate({
+                                        inputRange: [delay, ttl],
+                                        outputRange: [tyStart, tyEnd],
+                                        extrapolate: 'clamp'
+                                    })
+                                },
+                                {
+                                    translateX: animationValue.interpolate({
+                                        inputRange: [delay, ttl],
+                                        outputRange: [txStart, txEnd],
+                                        extrapolate: 'clamp'
+                                    })
+                                }
+                            ]
+                        }}
+                    />
+                );
+
+                // Quando a particula será removida
+                let deadts = now + (ttl * ANIM_DURATION);
+
+                this.allParticles.push([particle, deadts]);
+
+                // Adiciaona na lista das novas particulas a renderizar
+                particles.push(particle);
+
+                minTTL = Math.min(minTTL, ttl);
+            }
+
+            particles.sort((a: JSX.Element, b: JSX.Element) => {
+                return (a.key as string).localeCompare(b.key as string);
+            });
+
+            // Agenda proxima execução do script
+            setTimeout(this.addParticles, minTTL * ANIM_DURATION);
+
+            this.setState({
+                particles: particles
+            }, () => {
+
+                let animation = Animated.timing(animationValue, {
+                    toValue: 1,
+                    duration: ANIM_DURATION,
+                    useNativeDriver: true
+                });
+
+                this.animations.push(animation);
+
+                animation.start(result => {
+                    let idx = this.animations.indexOf(animation);
+                    if (idx >= 0) {
+                        this.animations.splice(idx, 1);
+                    }
+
+                    this.addParticles();
+                });
+            });
+        }
+    };
+
+    componentDidMount(): void {
+        this.addParticles();
+    }
+
+    componentWillUnmount() {
+        this.stop = true;
+        this.animations.forEach(value => {
+            value.stop();
+        })
+    }
 
     render() {
         const theme = getTheme();
@@ -44,9 +237,17 @@ export default class GradientHeader extends React.PureComponent<GradientHeaderPr
                     {...this.props}
                     header={(
                         <Animated.View style={[{opacity: headerOpacityColor}]} onLayout={this.onLayoutHeader}>
+                            <View style={[StyleSheet.absoluteFill]} pointerEvents={'none'}>
+                                {
+                                    this.state.particles.map(value => {
+                                        return value;
+                                    })
+                                }
+                            </View>
                             <EmptyState
                                 title={this.props.title}
                                 titleProps={{color: '#FFF'}}
+
                                 description={
                                     (typeof this.props.description === 'string')
                                         ? (
---
+++ example/src/pages/AnimatedModalPage.tsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import {View,} from 'react-native';
+import {NavigationScreenProp} from 'react-navigation';
+import {NavigationHeaderOptions} from "../components/NavigationHeader";
+import {Button, getTheme, SimpleText, TableViewRow} from "rn-components-ui";
+import GradientHeader from "../components/GradientHeader";
+import Code from "../components/Code";
+
+
+export type AnimatedModalPageProps = {
+    navigation: NavigationScreenProp<any, {}>
+}
+
+export default class AnimatedModalPage extends React.PureComponent<AnimatedModalPageProps> {
+
+    static navigationOptions = (config: any): NavigationHeaderOptions => {
+        return {
+            title: 'AnimatedModal'
+        };
+    };
+
+    render() {
+        const theme = getTheme();
+        let SEQ = 1;
+
+        return (
+            <GradientHeader
+                title={'AnimatedModal'}
+                description={'A button consists of text that clearly communicates what action will occur when the user touches it. The two main types of buttons used are Process buttons, and Ancillary buttons.'}
+                sections={[
+                    {
+                        key: `section-${SEQ++}`,
+                        header: 'Import',
+                        data: [
+                            {
+                                key: `row-${SEQ++}`,
+                                title: (
+                                    <Code src={`import { Carousel } from "rn-components-ui";`}
+                                    />
+                                )
+                            }
+                        ] as Array<TableViewRow>
+                    },
+                    {
+                        key: `section-${SEQ++}`,
+                        header: 'Simple',
+                        data: [
+                            {
+                                key: `row-${SEQ++}`,
+                                title: (
+                                    <View>
+                                        <Button
+                                            title={'Submit Form'}
+                                            onPress={() => {
+                                            }}
+                                        />
+                                    </View>
+                                ),
+                                subtitle: (
+                                    <Code src={`
+                                                <Button
+                                                    title={'Submit Form'}
+                                                    onPress={() => {
+                                                    }}
+                                                />
+                                            `}
+                                    />
+                                )
+                            }
+                        ] as Array<TableViewRow>
+                    },
+                    {
+                        key: `section-${SEQ++}`,
+                        header: 'Usage',
+                        data: [
+                            {
+                                key: 'issues-feedback',
+                                title: (
+                                    <View>
+                                        <SimpleText align={'justify'}>
+                                            {`The Button is used mainly when the action taken is the final step in a process (i.e. submitting an form). However, the Button may also be used for ancillary actions such as the Next button navigating to a new screen in the Walkthrough steps.`}
+                                        </SimpleText>
+
+                                        <SimpleText align={'justify'} style={{paddingTop: theme.padding}}>
+                                            {`When an action button is tapped if there are required fields that have not been filled out, a Toast should be shown indicating that there are required fields be filled out, and the screen will scroll to the fields in question. If there are mulitiple required fields yet to be filled out, scroll to the topmost one.`}
+                                        </SimpleText>
+                                    </View>
+                                )
+                            }
+                        ] as Array<TableViewRow>
+                    }
+                ]}
+            />
+        );
+    }
+}
